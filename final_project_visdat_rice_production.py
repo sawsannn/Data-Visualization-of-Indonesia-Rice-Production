511e6de6be3766b8d6799236e0d47fe38435cdad
sawsannn <90541825+sawsannn@users.noreply.github.com>
2025-06-05 21:46:15 +0700

    Add files via upload

--- final_project_visdat_rice_production.py
+++ final_project_visdat_rice_production.py
@@ -3,8 +3,8 @@ import streamlit as st
 from bokeh.plotting import figure
 from bokeh.models import HoverTool, ColumnDataSource
 from bokeh.palettes import Category10
-from streamlit_bokeh import streamlit_bokeh
 from bokeh.models import NumeralTickFormatter
+from streamlit_bokeh import streamlit_bokeh
 
 # Load data and set index
 data = pd.read_csv(
@@ -12,28 +12,24 @@ data = pd.read_csv(
 )
 data.set_index('Tahun', inplace=True)
 
-# # Rename columns for easier use
-# data.rename(columns={
-#     'Suhu rata-rata': 'suhu_rata',
-#     'Curah hujan': 'curah_hujan',
-#     'Luas Panen': 'luas_panen'
-# }, inplace=True)
+# Rename columns for easier use
+data.rename(columns={
+    'Suhu rata-rata': 'suhu_rata',
+    'Curah hujan': 'curah_hujan',
+    'Luas Panen': 'luas_panen'
+}, inplace=True)
 
 # Variables to plot
-# variables = ['Produksi', 'luas_panen', 'curah_hujan', 'Kelembapan', 'suhu_rata']
-variables = ['Produksi', 'Luas Panen', 'Curah hujan', 'Kelembapan', 'Suhu rata-rata']
+variables = ['Produksi', 'luas_panen', 'curah_hujan', 'Kelembapan', 'suhu_rata']
 colors = Category10[len(variables)]
 
 # Province list for dropdown
 prov_list = data['Provinsi'].unique().tolist()
 
-st.title("Indonesia Rice Production - Line Plot")
+st.title("Indonesia Rice Production - Interactive Line Plot")
 
 # Dropdown to select province
 province = st.selectbox("Select Province:", prov_list)
-
-# Dropdown to select variable (only one line shown)
-selected_var = st.selectbox("Select Variable to Plot:", variables)
 
 # Filter data for chosen province & reset index so 'Tahun' is column
 df_prov = data[data['Provinsi'] == province].reset_index()
@@ -43,30 +39,27 @@ source = ColumnDataSource(df_prov)
 
 # Create Bokeh figure
 p = figure(
-    title=f"{selected_var} Trend for {province}",
+    title=f"Yearly Trends for {province}",
     x_axis_label='Year',
-    y_axis_label=selected_var,
+    y_axis_label='Value',
     width=800,
     height=500,
     tools="pan,wheel_zoom,reset,save"
 )
 
-# Format y-axis with commas instead of scientific notation
+# Format y-axis numbers with commas instead of scientific notation
 p.yaxis.formatter = NumeralTickFormatter(format="0,0")
 
-# Add hover tool
-hover = HoverTool(tooltips=[
-    ("Year", "@Tahun"),
-    (selected_var, f"@{{{selected_var}}}")
-])
+# Add hover tool with all variables
+tooltips = [("Year", "@Tahun")]
+tooltips += [(var, f"@{{{var}}}") for var in variables]
+hover = HoverTool(tooltips=tooltips)
 p.add_tools(hover)
 
-# Get color for selected variable
-color = colors[variables.index(selected_var)]
-
-# Plot single line and circles for selected variable
-p.line('Tahun', selected_var, source=source, line_width=3, color=color, legend_label=selected_var)
-p.circle('Tahun', selected_var, source=source, size=8, fill_color="white", color=color)
+# Plot a line and circle for each variable
+for i, var in enumerate(variables):
+    p.line('Tahun', var, source=source, line_width=2, color=colors[i], legend_label=var)
+    p.circle('Tahun', var, source=source, fill_color="white", size=6, color=colors[i])
 
 p.legend.location = "top_left"
 p.legend.click_policy = "hide"
